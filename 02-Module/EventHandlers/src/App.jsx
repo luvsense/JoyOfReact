83bb2fcae03a8a71652b441ff6c4289ee2c543c9
nstlgy <yvsh7.btw@gmail.com>
2024-05-25 04:53:06 +0530

    new changes

---
+++ 02-Module/EventHandlers/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  function chips() {
+    console.log("i love chips hehe");
+  }
+  return (
+    <>
+      <button onClick={chips()}>Click Me!</button>
+    </>
+  );
+}
+
+export default App;
